2db8b00dff230fd4ffa7982c76923497508a142e
megurram <megurram@CHNSIPDT0T717>
2019-05-23 17:55:33 +0530

    added

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -11,7 +11,7 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Some Changes");
+            Console.WriteLine("Some Changed");
             Console.ReadKey();
         }
     }
